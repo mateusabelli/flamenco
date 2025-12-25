3965fc236605ab1cb3768e578d2e977ea92c913f
Sybren A. Stüvel <sybren@blender.org>
2025-12-25 13:50:27 +0100

    Bumped version down to 3.8.1

--- magefiles/version.go
+++ magefiles/version.go
@@ -11,8 +11,8 @@ import (
 // To update the version number in all the relevant places, update the VERSION
 // variable below and run `make update-version`.
 const (
-	version      = "3.9-alpha1"
-	releaseCycle = "alpha"
+	version      = "3.8.1"
+	releaseCycle = "release"
 )
 
 func gitHash() (string, error) {
