af916c190f134517f208911d911cba5ed26e7683
Sybren A. Stüvel <sybren@blender.org>
2026-03-26 15:20:10 +0100

    Bumped version to 3.9-alpha5

--- web/app/src/manager-api/ApiClient.js
+++ web/app/src/manager-api/ApiClient.js
@@ -55,7 +55,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'Flamenco/3.9-alpha4 / webbrowser'
+            'User-Agent': 'Flamenco/3.9-alpha5 / webbrowser'
         };
 
         /**
