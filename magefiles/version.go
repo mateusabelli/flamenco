f06c867b0ece019f2219491b0a786c263503ec94
Sybren A. Stüvel <sybren@blender.org>
2026-05-04 14:54:51 +0200

    Bumped version to 3.9.1-rc2

--- magefiles/version.go
+++ magefiles/version.go
@@ -11,7 +11,7 @@ import (
 // To update the version number in all the relevant places, update the VERSION
 // variable below and run `make update-version`.
 const (
-	version      = "3.9.1"
+	version      = "3.9.1-rc2"
 	releaseCycle = "rc"
 )
 
